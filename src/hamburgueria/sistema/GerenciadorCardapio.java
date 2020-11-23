f5a88ea25e440bf6f5377f485ef5f49e2815e7a2
gabrielASD123 <gabrielsev_2002@hotmail.com>
2020-11-23 00:29:01 -0300

    atualizados métodos para crud

--- src/hamburgueria/sistema/GerenciadorCardapio.java
+++ src/hamburgueria/sistema/GerenciadorCardapio.java
@@ -1,5 +1,26 @@
 package hamburgueria.sistema;
 
+import java.util.Scanner;
+
+import hamburgueria.DAO.SobremesaDAO;
+
 public class GerenciadorCardapio {
 
+	//salvar, apagar, atualizar.
+	
+	SobremesaDAO sobremesaDAO = new SobremesaDAO();
+	Scanner leitor = new Scanner(System.in);
+	
+	//Exemplo de salvar os itens da tabela sobremesas
+			/*
+				String nome;
+				int valor;
+				System.out.println("insira uma nova sobremesa");
+				nome = leitor.next();
+				System.out.println("insira o valor da nova sobremesa");
+				id = leitor.nextDouble();
+				Sobremesa sobremesa = new Sobremesa(nome,valor);
+				sobremesaDAO.salvar(sobremesa);
+			*/
+			
 }
--- src/hamburgueria/sistema/Programa.java
+++ src/hamburgueria/sistema/Programa.java
@@ -21,37 +21,42 @@ public class Programa {
 
 	public static void main(String[] args) throws Exception {
 		
+		//listar, listarPorId
 		//Observação, caso você escreva "HamburguerDAO.metodo();" um erro vai ocorrer porque é o nome do método estático, com letra maíscula
 		//A primeria letra deve ser minúscula para fazer o método de instância corretamente: nomeDoMetodoDAO.listar();
 	
-		SobremesaDAO sobremesaDAO = new SobremesaDAO();
+		//SobremesaDAO sobremesaDAO = new SobremesaDAO();
+		//Scanner leitor = new Scanner(System.in);
 		
 		//Exemplo de salvar os itens da tabela sobremesas
-		
+		/*
 			String nome;
-			double id;
-			Scanner leitor = new Scanner(System.in);
+			int valor;
 			System.out.println("insira uma nova sobremesa");
 			nome = leitor.next();
 			System.out.println("insira o valor da nova sobremesa");
 			id = leitor.nextDouble();
-			Sobremesa sobremesa = new Sobremesa(nome,id);
+			Sobremesa sobremesa = new Sobremesa(nome,valor);
+			sobremesaDAO.salvar(sobremesa);
+		*/
 		
 		//Exemplo apagar ...
-		
-			//sobremesaDAO.apagar(2);
-		
+		/*
+			int id;
+			id = leitor.nextInt();
+			sobremesaDAO.apagar(id);
+		*/
 		//Exemplo de listar ...
 		/*
 				List<Sobremesa> listaS = sobremesaDAO.listar();
 				
 					for (Sobremesa id : listaS) {
 					
-						System.out.println(id);								
+						System.out.println(id.getValor());								
 					}
-			*/
+		*/	
 		//Exemplo burcarPorId ...
-		/*
+		
 		Integer id = 0;
 		Scanner leitor = new Scanner(System.in);
 		System.out.println("insira um id para ver a sobremesa");
@@ -59,7 +64,7 @@ public class Programa {
 		
 			SobremesaDAO sobremesaDAO = new SobremesaDAO();
 			System.out.println(sobremesaDAO.buscarPorId(id));
-		*/
+		
 
 	}
 }
